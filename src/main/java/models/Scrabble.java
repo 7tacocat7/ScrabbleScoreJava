4f1e7773ee7e49b2310f7415b8778097f60b86d1
Carson Canales and Kira Sarnoff-Loo <kira@>
2017-08-01 11:54:46 -0700

    created initial files and directories

---
+++ src/main/java/models/Scrabble.java
@@ -0,0 +1,14 @@
+package models;
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * Created by Guest on 8/1/17.
+ */
+public class Scrabble {
+
+    public Integer calculateScore(String input){
+        Map<Character, Integer> scrabbleLetters = new HashMap<Character, Integer>();
+        scrabbleLetters.put('a', 1);
+    }
+}
